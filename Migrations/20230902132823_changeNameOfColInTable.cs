fa9772e1e6a5cbbc817cf015e573ae81ff100d5b
Amal <Amal@DESKTOP-BBB3TT7>
2023-09-03 12:25:07 +0300

    Add project files.

---
+++ Migrations/20230902132823_changeNameOfColInTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeNameOfColInTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "ProductDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Model",
+                table: "ProductDetails",
+                newName: "Flavor");
+
+            migrationBuilder.RenameColumn(
+                name: "Color",
+                table: "Carts",
+                newName: "Flavor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Flavor",
+                table: "ProductDetails",
+                newName: "Model");
+
+            migrationBuilder.RenameColumn(
+                name: "Flavor",
+                table: "Carts",
+                newName: "Color");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "ProductDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Migrations/DashboardDb/20230902135918_newAuth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dashboard.Migrations.DashboardDb
+{
+    /// <inheritdoc />
+    public partial class newAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
